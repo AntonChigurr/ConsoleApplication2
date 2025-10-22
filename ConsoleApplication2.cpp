1422cda25ae755ed5a770b31a44fa4425196b3d7
User <User@LENOVO>
2025-10-22 15:26:12 +0400

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int* ptr = new int;
+    *ptr = 42;     
+    cout << "number: " << *ptr << endl;
+
+    delete ptr;       
+    ptr = nullptr;    
+    return 0;
+}
